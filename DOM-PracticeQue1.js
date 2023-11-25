67a3d424372e792abd2cfeab78b9b66dd51dff95
Princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-25 17:29:11 +0530

    DOM-Practice Question.1 JavaScript CODE
    // Create a h2 heading element with text - "JavaScript". Append "with Prince Panchal" to this text using JS.

---
+++ DOM-PracticeQue1.js
@@ -0,0 +1,6 @@
+// Create a h2 heading element with text - "JavaScript". Append "with Prince Panchal" to this text using JS.
+
+let h2 = document.querySelector("h2");
+console.dir(h2);
+
+h2.innerText = h2.innerText + " with Prince Panchal";
